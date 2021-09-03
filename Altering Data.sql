701da8b6fd2eb7fa0e972c2c346f3e6180019bdd
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-09-03 18:43:28 +0530

    Update Altering Data.sql

--- Altering Data.sql
+++ Altering Data.sql
@@ -4,3 +4,9 @@ ADD leaving_date varchar(50);
 
 ALTER TABLE employee
 MODIFY COLUMN leaving_date DATETIME;
+
+ALTER TABLE employee
+CHANGE leaving_date leaving DATETIME;
+
+ALTER TABLE employee
+DROP COLUMN leaving;
